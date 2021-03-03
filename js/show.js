d9554d53e8d7b12821fa4b2b2620f599918c8bdf
odwanodada <odwanodada22@gmail.com>
2021-03-03 15:51:16 +0200

    picsappear

--- js/show.js
+++ js/show.js
@@ -1,27 +1,24 @@
 function getAlldata() {
-  // Get element to change
-  let list = document.getElementById("container");
-
   // Fetch the data
   fetch("http://127.0.0.1:5000/show-items/")
     .then((response) => response.json())
     .then((json) => {
       console.log(json);
-      json.forEach((card) => createBlogItem(card));
+      json.forEach((card) => displayItems(card));
     });
 }
 
-function createBlogItem(Books) {
-  const item = `
-    <div class="column">
-    <div class="card">
-      <h3>${Books.Title}</h3>
-      <p>${Books.Author}</p></p>
-     
-   </div>
- </div>
-`;
-  let list = document.getElementsByClassName("container");
+function displayItems(Items) {
+  const item = `<div id="card" class="content">
+                     <img src="${Items.Images}"/>
+                     <h3>${Items.Title}</h3>
+                     <p>${Items.Author}</p>
+                     <p>${Items.Genres}</p>
+                     <p>${Items.Originally_published}</p>
+                     <p>R${Items.Price}</p>
+                   <button class="buy">Buy Now</button>
+                </div> `;
+  let list = document.getElementById("container");
   list.innerHTML += item;
 }
 
